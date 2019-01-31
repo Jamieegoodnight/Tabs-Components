ed82e3a97562d7a08e959d047b73d991412e0b5a
Jamie Goodnight <goodnightlambda@gmail.com>
2019-01-31 16:41:44 -0500

    Added a tab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -34,7 +34,7 @@ class TabLink {
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 
-    links.forEach(link => link.classList.remove('tabs-link-selected'));
+    links.forEach(x => x.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
@@ -59,7 +59,7 @@ class TabItem {
     const items=document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    items.forEach(item => item.classList.remove('tabs-item-selected'));
+    items.forEach(x => x.classList.remove('tabs-item-selected'));
     
     // Add a class named "tabs-item-selected" to this element
     //this.element;
@@ -75,6 +75,6 @@ let links=document.querySelectorAll('.tabs-link');
 
 // - With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
 
-links.forEach(link => new TabLink(link));
+links.forEach(x => new TabLink(x));
 
 // - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
